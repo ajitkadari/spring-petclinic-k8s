044d56617aa2d2f8cb6a816a8f3d44f6ec8764a6
ajitkadari <ajit.kadari@outlook.com>
2024-11-07 21:27:39 -0600

    @Counted aspect registration

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/config/ObservationConfiguration.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/config/ObservationConfiguration.java
@@ -5,6 +5,7 @@ import io.micrometer.observation.Observation.Context;
 import io.micrometer.observation.ObservationHandler;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import io.micrometer.core.aop.CountedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import io.micrometer.core.instrument.observation.DefaultMeterObservationHandler;
@@ -20,7 +21,7 @@ public class ObservationConfiguration {
 
     // Monitoring beans
     @Bean
-    ObservationRegistry observationRegistry() {
+    public ObservationRegistry observationRegistry() {
       final var observationRegistry = ObservationRegistry.create();
       observationRegistry.observationConfig()
         .observationHandler(tracingAwareMeterObservationHandler())
@@ -45,5 +46,11 @@ public class ObservationConfiguration {
     @Bean
     ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
       return new ObservedAspect(observationRegistry);
+    }
+
+    // To have the @Counted support we need to register this aspect
+    @Bean
+    public CountedAspect countedAspect(MeterRegistry registry) {
+        return new CountedAspect(registry);
     }
 }
